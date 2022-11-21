50dbf9f479a0aac2d635dbca5e68d79ec711719f
kaushik1904 <kaushikprajapati1970@gmail.com>
2022-11-22 05:21:01 +0530

    page not found page added

---
+++ src/component/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./PageNotFound.scss";
+
+const PageNotFound = () => {
+  return (
+    <div id="mainC">
+      <div class="message">
+        <h1>404</h1>
+        <h3>the page you seek does not exist</h3>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
