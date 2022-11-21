38a839db1e4ce39d4d527fe80374b728eb8411ff
kaushik1904 <kaushikprajapati1970@gmail.com>
2022-11-22 05:21:33 +0530

    loading component added

---
+++ src/component/Loading/Loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Loading.scss";
+
+const Loading = () => {
+  return (
+    <div id="outer">
+      <div id="middle">
+        <div id="inner"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
